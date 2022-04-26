591f9596aaa8cd12cb425a0090929039a7c299a6
Dongmo Baudouin <Dongmo Baudouin>
2022-04-26 15:41:07 +0100

    creation du repository pattern

---
+++ PatternRepository/IRepository.cs
@@ -0,0 +1,67 @@
+﻿using DevoirRest.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace DevoirRest.PatternRepository
+{
+    /// <summary>
+    ///     A genercic repository pattern interface
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IRepository<T> where T: class
+    {
+        /// <summary>
+        /// Inserts a single entity into the DbSet
+        /// </summary>
+        /// <param name="entity"></param>
+        void Insert(T entity);
+
+
+        /// <summary>
+        /// Inserts Range entities into the DbSet
+        /// </summary>
+        /// <param name="entities"></param>
+        void InsertRange(List<T> entities);
+
+
+        /// <summary>
+        /// Returns an entity based on primary key.
+        /// </summary>
+        /// <param name="id">Primary Key.</param>
+        /// <returns></returns>
+        T GetById(int id);
+
+        /// <summary>
+        /// Returns an entity based on primary key.
+        /// </summary>
+        /// <param name="id">Primary Key.</param>
+        /// <returns></returns>
+        List<T> GetAll();
+        List<T> GetAll(bool active);
+
+
+
+        /// <summary>
+        /// Returns an IEnumerable based on the query, order clause and the properties included
+        /// </summary>
+        /// <param name="query">Link query for filtering.</param>
+        /// <param name="orderBy">Link query for sorting.</param>
+        /// <param name="includeProperties">Navigation properties seperated by comma for eager loading.</param>
+        /// <returns>IEnumerable containing the resulting entity set.</returns>
+        IEnumerable<T> GetByQuery(Expression<Func<T, bool>> query = null,
+                                  Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+                                  string includeProperties = "");
+
+
+        /// <summary>
+        /// Updates an entity.
+        /// </summary>
+        /// <param name="entity"></param>
+        void Update(T entity);
+
+
+    }
+}
---
+++ PatternRepository/Repository.cs
@@ -0,0 +1,126 @@
+﻿using DevoirRest.Context;
+using DevoirRest.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace DevoirRest.PatternRepository
+{
+
+    public class Repository<T> : IRepository<T> where T : BaseModel
+    {
+        public static readonly ApplicationDBContext _ctx = null;
+        public static ApplicationDBContext Ctx
+        {
+            get
+            {
+                var optionsBuilder = new DbContextOptionsBuilder<ApplicationDBContext>();
+
+                return _ctx ?? new ApplicationDBContext(optionsBuilder.Options);
+            }
+        }
+
+        /// <summary>
+        /// Underlying DbSet.
+        /// </summary>
+        protected DbSet<T> _DbSet;
+
+        /// <summary>
+        /// Underlying dbcontext
+        /// </summary>
+        protected DbContext _DbContext;
+
+
+        /// <summary>
+        /// Underlying queryable
+        /// </summary>
+        protected IQueryable<T> _Query;
+
+        public Repository(DbContext dataContext, IQueryable<T> query = null)
+        {
+            _DbContext = dataContext;
+            _DbSet = _DbContext.Set<T>();
+            if (query != null)
+            {
+                _Query = query;
+            }
+        }
+        public List<T> GetAll()
+        {
+            return GetByQuery().ToList<T>();
+        }
+
+        public List<T> GetAll(bool active)
+        {
+            return GetByQuery(t => t.IsActive.Equals(active)).ToList<T>();
+        }
+
+        public T GetById(int id)
+        {
+            return _DbSet.Find(id);
+        }
+
+        public IEnumerable<T> GetByQuery(Expression<Func<T, bool>> query = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, string includeProperties = "")
+        {
+
+            IQueryable<T> queryResult = null;
+            if (queryResult == null)
+            {
+                queryResult = _DbSet;
+            }
+
+
+            //If there is a query, execute it against the dbset
+            if (query != null)
+            {
+                queryResult = queryResult.Where(query);
+            }
+
+            //get the include requests for the navigation properties and add them to the query result
+            foreach (var property in includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+            {
+                queryResult = queryResult.Include(property);
+            }
+
+            //if a sort request is made, order the query accordingly.
+            if (orderBy != null)
+            {
+                return orderBy(queryResult).ToList();
+            }
+            //if not, return the results as is
+            else
+            {
+                return queryResult.ToList();
+            }
+        }
+
+        public void Insert(T entity)
+        {
+            _DbSet.Add(entity);
+            _DbContext.Entry(entity).State = EntityState.Added;
+            _DbContext.SaveChanges();
+        }
+
+        public void InsertRange(List<T> entities)
+        {
+            _DbSet.AddRange(entities);
+            foreach (T entity in entities)
+                _DbContext.Entry(entity).State = EntityState.Added;
+            _DbContext.SaveChanges();
+        }
+
+        public void Update(T entity)
+        {
+            if (_DbContext.Entry(entity).State == EntityState.Detached)
+            {
+                _DbSet.Attach(entity);
+            }
+
+            _DbContext.Entry(entity).State = EntityState.Modified;
+            _DbContext.SaveChanges();
+        }
+    }
+}
